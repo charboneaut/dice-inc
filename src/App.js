0392a3c3ec7f729eb090b3694171bb298fa080e4
Timothy Charboneau <charboneau@protonmail.ch>
2021-02-04 09:56:25 -0500

    added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import GameContainer from "./comps/GameContainer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
--- src/comps/GameContainer.js
+++ src/comps/GameContainer.js
@@ -1,9 +1,15 @@
 import "./GameContainer.css";
 import { Component } from "react";
+import GameBar from "./GameBar";
 
 class GameContainer extends Component {
   render() {
-    return <div className="gameContainer">This is the game container</div>;
+    return (
+      <div className="gameContainer">
+        <GameBar />
+        This is the game container
+      </div>
+    );
   }
 }
 
